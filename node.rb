7e8556b2a30e0d536f0928f46185c43e0daa0055
RobotOptimist <james.macivor1@gmail.com>
2014-09-30 07:42:05 -0400

    binary tree combine
    a simple google search reveals I might be doing this all wrong

--- node.rb
+++ node.rb
@@ -1,33 +1,45 @@
 module BinaryTree
 	class Node
-		attr_accessor :node
+		
 		def initialize (num)
 			@node = {:value => num, :left_child => nil, :right_child => nil}			
 		end
 		
-		def get_parent
+		def parent
 			@parent
 		end
 		
-		def get_node
+		def node
 			@node
 		end
 		
-		def set_parent(parent)
+		def value
+			@node[:value]
+		end
+		
+		def left_child
+			@node[:left_child]
+		end
+		
+		def right_child
+			@node[:right_child]
+		end
+		
+		def parent=(parent)
 			return nil if parent.class != Node
 			@parent = parent
 		end
 		
-		def set_left_child(child)
+		def set_left_child=(child)
 			return nil if child.class != Node
 			@node[:left_child] = child
-			child.set_parent(self)
+			child.parent = self
 		end
 		
-		def set_right_child(child)
+		def set_right_child=(child)
 			return nil if child.class != Node
 			@node[:right_child] = child	
-			child.set_parent(self)
+			child.parent = self
 		end
 	end
 
@@ -36,7 +48,7 @@ module BinaryTree
 		def build_tree(arr)
 			arrs = split_array(arr)			
 			nodes = convert_to_nodes(arrs)
-			p nodes[0].get_node[:left_child]
+			p nodes[0].left_child.value
 		end
 		
 		def split_array(arr)
@@ -73,19 +85,50 @@ module BinaryTree
 			node = Node.new(arr[1])			
 			left = Node.new(arr[0])	
 			right = Node.new(arr[2])
-			node.set_left_child(left)
-			node.set_right_child(right)
+			node.set_left_child = left
+			node.set_right_child = right
 			node
 		end
 		
 		def two_el_node(arr)
 			node = Node.new(arr[0])
 			right = Node.new(arr[1])
-			node.set_right_child(right)
+			node.set_right_child = right
 		end
 		
 		def one_el_node(arr)
 			node = Node.new(arr[0])
+		end
+		
+		def combine_nodes(node,nodet)
+			
+			
+		end
+		
+		def assign_children(node,nodet)
+		
+		end
+		
+		def sort_nodes(node,nodet)
+			if node.value < nodet.value
+				small = node
+				large = nodet
+			else
+				small = nodet
+				large = node
+			end			
+			if large.left_child = nil || large.left_child.value < small.value
+				return [small,large]
+			else
+				large = large.left_child
+				sort_nodes(large,small)							
+			end
+		end
+		
+		def analyze_nodes(node,nodet)
+			ovalue = node.value; oleftv = node.left_child.value;	orightv = node.right_child.value;	
+			tvalue = nodet.value;	tleftv = nodet.left_child.value; trightv = nodet.right_child.value
+			
 		end
 		
 		def breadth_first_search
